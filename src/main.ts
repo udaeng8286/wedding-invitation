df2fe05e4c281e219245a6cc4541be12867115eb
Song Yu Jeong <udaenglvup@gmail.com>
2025-07-27 13:42:43 +0900

    메인 색상 변경

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,10 @@ import PrimeVue from "primevue/config";
 import App from "./App.vue";
 import Galleria from "primevue/galleria";
 
-// ✅ CSS import
 import "primevue/resources/themes/lara-light-indigo/theme.css";
 import "primevue/resources/primevue.min.css";
 import "primeicons/primeicons.css";
-
+import "./assets/styles/main.css";
 const app = createApp(App);
 app.use(PrimeVue);
 app.component("Galleria", Galleria);
